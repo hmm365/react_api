80404890d1596646126bbb16beca38c3aaec0889
hmm365 <happyham52@gmail.com>
2022-11-03 19:10:09 +0900

    1103

--- src/components/include/MovieCont.jsx
+++ src/components/include/MovieCont.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 
+const MoiveInfo = ({ title, voteAverage, poster, releasedate, originName }) => {
+	return (
+		<article className='card'>
+			<figure className='card__top'>
+				<a href='/'>
+					<img src={`https://image.tmdb.org/t/p/w500${poster}`} alt={`${originName} poster`} />
+				</a>
+			</figure>
+			<div className='card__bottom'>
+				<a className='title' href='/'>
+					{title}
+				</a>
+				<p className='vote_average'>평점 : {voteAverage}</p>
+				<p className='releasedate'>{releasedate}</p>
+			</div>
+		</article>
+	);
+};
+
 const MovieCont = ({ movies }) => {
+	console.log({ movies });
 	return (
 		<section className='cont__movie'>
 			<div className='container'>
 				<div className='movie__inner'>
-					<article className='card'>
-						<figure className='card__header'>
-							<img
-								src='https://images.unsplash.com/photo-1667312402040-2a656f309e4d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
-								alt='고양이 사진1'
-							/>
-						</figure>
-						<div className='card__body'>
-							<h3 className='tit'>저녁 에</h3>
-							<p className='desc'>
-								저렇게 많은 별 중에서
-								<br />
-								별 하나가 나를 내려다본다
-								<br />
-								이렇게 많은 사람 중에서
-								<br />그 별 하나를 쳐다본다
-							</p>
-						</div>
-					</article>
+					{movies.map((movie, idx) => (
+						<MoiveInfo
+							key={idx}
+							title={movie.title}
+							voteAverage={movie.vote_average}
+							poster={movie.poster_path}
+							releasedate={movie.release_date}
+							originName={movie.original_title}
+						/>
+					))}
 				</div>
 			</div>
 		</section>
