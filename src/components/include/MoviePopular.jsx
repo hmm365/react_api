a2d853198ec672dc564840373d729a04f669d337
hmm365 <happyham52@gmail.com>
2022-11-08 09:24:51 +0900

    11.08

--- src/components/include/MoviePopular.jsx
+++ src/components/include/MoviePopular.jsx
@@ -6,6 +6,7 @@ const MoiveItem = movie => {
 			<figure className='card__top'>
 				<a href={`https://www.themoviedb.org/movie/${movie.movieId}`}>
 					<img src={`https://image.tmdb.org/t/p/w500/${movie.poster}`} alt={`${movie.originName} poster`} />
+					<span>{movie.rank}</span>
 				</a>
 			</figure>
 			<div className='card__bottom'>
@@ -25,17 +26,20 @@ const MoviePopular = ({ popularMovies }) => {
 				<h2>Best Movie</h2>
 				<div className='movie__inner'>
 					<div className='movie__card__wrap'>
-						{popularMovies.map((movie, idx) => (
-							<MoiveItem
-								key={idx}
-								movieId={movie.id}
-								title={movie.title}
-								voteAverage={movie.vote_average}
-								poster={movie.poster_path}
-								releasedate={movie.release_date}
-								originName={movie.original_title}
-							/>
-						))}
+						{popularMovies.map((movie, idx) =>
+							idx < 10 ? (
+								<MoiveItem
+									key={idx}
+									rank={idx + 1}
+									movieId={movie.id}
+									title={movie.title}
+									voteAverage={movie.vote_average}
+									poster={movie.poster_path}
+									releasedate={movie.release_date}
+									originName={movie.original_title}
+								/>
+							) : null
+						)}
 					</div>
 				</div>
 			</div>
