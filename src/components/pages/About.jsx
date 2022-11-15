493ce8f3982be57a05948758e4d0400139637679
hmm365 <happyham52@gmail.com>
2022-11-15 14:25:40 +0900

    1115

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Contents from '../layout/Contents';
 import Footer from '../layout/Footer';
 import Header from '../layout/Header';
@@ -7,12 +7,21 @@ import AboutCont from '../include/AboutCont';
 import Contact from '../layout/Contact';
 
 export const About = () => {
+    const [abouts, setAbouts] = useState([]);
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/search/movie?api_key=891244fed71f3e78a463eb2a1801b383&language=ko-kr&query=마블&page=1&include_adult=false')
+            .then((response) => response.json())
+            // .then(result => console.log(result.results))
+            .then((result) => setAbouts(result.about))
+            .catch((error) => console.log('error', error));
+    }, []);
+
     return (
         <>
             <Header />
             <Contents>
                 <Title title={['ABOUT', 'ME']} />
-                <AboutCont />
+                <AboutCont abouts={abouts} />
                 <Contact />
             </Contents>
             <Footer />
