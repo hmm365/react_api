af8ba992552dbb9b760bbbe22a688b74b7b9da52
hmm365 <happyham52@gmail.com>
2022-11-15 13:35:55 +0900

    1115

--- src/components/include/AboutCont.jsx
+++ src/components/include/AboutCont.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
 
 const AboutCont = () => {
-	return <section className='cont__about'></section>;
+    return (
+        <section className='cont__about'>
+            <div className='container'>
+                <h1>
+                    <strong>build Up</strong>
+                    <em>Developer</em>
+                </h1>
+                <p>
+                    안녕하세요. 프론트엔드를 꿈꾸는 이중호 입니다. 저의 포트폴리오에 오신 것을 환영합니다. 부족한 디자인을 보완하려 웹반응형UI/UX 웹
+                    퍼블리셔과정을 배우게 되었는데 웹페이지에서 동적인 것을 구현할 수 있는 프론트엔드 개발에 점차 흥미를 느끼게 됐습니다.
+                </p>
+                <img src='../../utils/img/myPp.png' alt='' />
+            </div>
+        </section>
+    );
 };
 
 export default AboutCont;
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Contents from "../layout/Contents";
-import Footer from "../layout/Footer";
-import Header from "../layout/Header";
-import Title from "../layout/Title";
-import AboutCont from "../include/AboutCont";
-import Contact from "../layout/Contact";
+import React from 'react';
+import Contents from '../layout/Contents';
+import Footer from '../layout/Footer';
+import Header from '../layout/Header';
+import Title from '../layout/Title';
+import AboutCont from '../include/AboutCont';
+import Contact from '../layout/Contact';
 
 export const About = () => {
-	return (
-		<>
-			<Header />
-			<Contents>
-				<Title title={["about", "referce api"]} />
-				<AboutCont />
-				<Contact />
-			</Contents>
-			<Footer />
-		</>
-	);
+    return (
+        <>
+            <Header />
+            <Contents>
+                <Title title={['ABOUT', 'ME']} />
+                <AboutCont />
+                <Contact />
+            </Contents>
+            <Footer />
+        </>
+    );
 };
